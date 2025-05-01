b49eab4dc9d67761646806f92da87421311abd7e
Bledi Dilaveri <bledid@fornova.net>
2025-05-01 19:00:46 +0200

    fix sc

--- StreamingCommunityProvider/src/main/kotlin/com/milanbota/StreamingCommunityProvider.kt
+++ StreamingCommunityProvider/src/main/kotlin/com/milanbota/StreamingCommunityProvider.kt
@@ -67,7 +67,7 @@ fun getImageUrl(mainUrl: String, url: String): String {
 }
 class StreamingCommunityProvider : MainAPI() {
     override var lang = "it"
-    override var mainUrl = "https://streamingcommunity.airforce"
+    override var mainUrl = "https://streamingcommunity.spa"
     override var name = "StreamingCommunity"
     override val hasMainPage = true
     override val hasChromecastSupport = true
